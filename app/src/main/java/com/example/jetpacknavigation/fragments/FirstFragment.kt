55be1c4a4b3bc75738e7a7ad3b4bfe98d899c9d1
Gagandeip Singh <gagandeips@gmail.com>
2024-06-21 13:25:17 +0530

    Random Number,Email Validations,doOnTextChanged

---
+++ app/src/main/java/com/example/jetpacknavigation/fragments/FirstFragment.kt
@@ -0,0 +1,99 @@
+package com.example.jetpacknavigation.fragments
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.navigation.fragment.findNavController
+import com.example.jetpacknavigation.R
+import com.example.jetpacknavigation.databinding.FragmentFirstBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentFirstBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.gtOtp.setOnClickListener {
+            val emailRegex = Regex("^[\\w.-]+@[\\w.-]+\\.\\w{2,}$")
+            if(binding.emailValue.text.toString() == ""){
+                binding.emailLayout.error = "Enter Email.."
+            }
+            else if(!binding.emailValue.text.toString().matches(emailRegex)){
+                binding.emailLayout.error = "Enter Correct Email.."
+            }
+            else{
+                val randomNum = generateRandomFourDigitNumber().toString()
+                val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:")
+                    putExtra(Intent.EXTRA_EMAIL,arrayOf(binding.emailValue.text.toString()))
+                    putExtra("subject", "OTP Request")
+                    val bodyText = getString(R.string.bodyTxt, randomNum)
+                    putExtra("body", bodyText)
+                }
+                startActivity(emailIntent)
+                findNavController().navigate(R.id.secondFragment)
+            }
+        }
+        binding.emailValue.doOnTextChanged { _, _, _, _ ->
+            binding.emailLayout.error = null
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FirstFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FirstFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+    private fun generateRandomFourDigitNumber(): Int {
+        val random = java.util.Random()
+        return random.nextInt(8889) + 1111
+    }
+}
---
+++ app/src/main/java/com/example/jetpacknavigation/fragments/SecondFragment.kt
@@ -0,0 +1,96 @@
+package com.example.jetpacknavigation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import com.example.jetpacknavigation.databinding.FragmentSecondBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.et1.doOnTextChanged { _, _, _, _ ->
+            if(binding.et1.text.toString().length == 1){
+                binding.et2.requestFocus()
+            }
+        }
+        binding.et2.doOnTextChanged { _, _, _, _ ->
+            if(binding.et2.text.toString().length == 1){
+                binding.et3.requestFocus()
+            }
+            if(binding.et2.text.toString() == ""){
+                binding.et1.requestFocus()
+            }
+        }
+        binding.et3.doOnTextChanged { _, _, _, _ ->
+            if(binding.et3.text.toString().length == 1){
+                binding.et4.requestFocus()
+            }
+            if(binding.et3.text.toString() == ""){
+                binding.et2.requestFocus()
+            }
+        }
+        binding.et4.doOnTextChanged { _, _, _, _ ->
+            if(binding.et4.text.toString().length == 1){
+                binding.et4.clearFocus()
+                binding.chkOtp.performClick()
+            }
+            if(binding.et4.text.toString() == ""){
+                binding.et3.requestFocus()
+            }
+        }
+    }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SecondFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SecondFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
